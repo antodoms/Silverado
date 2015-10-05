a7363af4b1f20ed5d0715bd990fb7154edb44ba9
antodoms <antodoms@gmail.com>
2015-10-05 19:06:46 +1100

    small link correction

--- application/views/header.php
+++ application/views/header.php
@@ -6,18 +6,22 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html>
     <head>
         <!-- Imported stuff -->
-	<link rel='shortcut icon' href="<?php echo asset_url(); ?>images/logohorse.png">
-	<link rel="stylesheet" type="text/css" href="<?php echo asset_url(); ?>css/layout.css">
-	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Quicksand">
-	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Poiret+One">
-	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Josefin+Sans">
-	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+		<link rel='shortcut icon' href="images/logohorse.png">
+		<link rel="stylesheet" type="text/css" href="layout.css">
+		<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Quicksand">
+		<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Poiret+One">
+		<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Josefin+Sans">
+		<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1, maximum-scale=1">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1, maximum-scale=1">
 
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-	<script src="<?php echo asset_url(); ?>js/scripts.js"></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular.min.js"></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.7/angular-resource.min.js"></script>
+
+		<script src="<?php echo asset_url(); ?>js/cookie.js"></script>
+		<script src="<?php echo asset_url(); ?>js/scripts.js"></script>
 
 	<title> Silverado | People's Own Theatre</title>
     </head>
