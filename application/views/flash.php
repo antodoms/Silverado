97bdf1bc8d46fe39ad3ee76dc1735706744035c7
antodoms <antodoms@gmail.com>
2015-10-13 06:20:01 +1100

    corrected flash message support to coreteaching server

--- application/views/flash.php
+++ application/views/flash.php
@@ -1,6 +1,6 @@
 <?php
     
-    if(method_exists($this->session->flashdata('flash'))) {
+    if(method_exists($this->session->flashdata('flash'),'read')) {
         $message = $this->session->flashdata('flash');
         echo '<div id="flash" class="'.$message['class'].'">'.$message['message'];
         echo '<button class="closex" onclick="removeflash()" >X</button></div>';
